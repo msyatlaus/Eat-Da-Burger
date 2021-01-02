00d4bef5937b7ab33fa9a2acab8008b218f4fb91
Bradley Guidry <bradleyguidry@gmail.com>
2021-01-02 15:48:21 -0600

    npm install and fomatting

--- Main/Eat-Da-Burger/public/assets/js/script.js
+++ Main/Eat-Da-Burger/public/assets/js/script.js
@@ -1,17 +1,14 @@
-$(document).ready(function() {
-    
-  $(".devour-form").on("submit", function(event) {
+$(document).ready(function () {
+  $(".devour-form").on("submit", function (event) {
     event.preventDefault();
 
     var burger_id = $(this).children(".burger_id").val();
     console.log(burger_id);
     $.ajax({
       method: "PUT",
-      url: "/burgers/" + burger_id
-    }).then(function(data) {
-      // reload page to display devoured burger in proper column
+      url: "/burgers/" + burger_id,
+    }).then(function (data) {
       location.reload();
     });
-
   });
 });
