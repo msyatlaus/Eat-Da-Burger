df0a6af9cfb8e3671528d165ae6e734b99c86083
Bradley Guidry <bradleyguidry@gmail.com>
2021-01-02 19:22:19 -0600

    jawsdb connection

--- Main/Eat-Da-Burger/config/connection.js
+++ Main/Eat-Da-Burger/config/connection.js
@@ -1,13 +1,23 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
+// var connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: "root",
+//   password: "Min3wc0de!",
+//   database: "burgers_db"
+// });
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
   port: 3306,
   user: "root",
   password: "Min3wc0de!",
   database: "burgers_db"
-});
-
+  });
+};
 
 connection.connect(function(err) {
   if (err) {
